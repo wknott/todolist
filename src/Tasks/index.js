9bb86cf012bc7c63d2a68ed5e5c47d6d9afd2976
Wojciech Knott <wojciechknott@gmail.com>
2020-07-11 00:24:36 +0200

    added done and delete icons

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./styles.css";
+import doneImage from "./done.svg";
+import deleteImage from "./delete.svg";
 
 const Tasks = ({ tasks, isShowingEnabled }) => (
   <ul className="list">
@@ -9,15 +11,15 @@ const Tasks = ({ tasks, isShowingEnabled }) => (
         className={`list__item ${isShowingEnabled || !done ? "" : "list__item--hide"}`}
       >
         <button className="list__button list__button--done">
-          {done ? "✔" : " "}
+          {done
+            ? <img height={15} src={doneImage} alt="done" />
+            : ""}
         </button>
         <p className="list__paragraph">
           {done ? <s>{name}</s> : name}
         </p>
         <button className="list__button list__button--remove ">
-          <span role="img" aria-label="Delete">
-            ❌
-          </span>
+          <img height={15} src={deleteImage} alt="delete" />
         </button>
       </li>
     ))}
