8ab5ddbd6040c7874e90e86f64cc2d2fe30b8505
Wojciech Knott <wojciechknott@gmail.com>
2020-07-10 16:03:53 +0200

    Updates

---
+++ precache-manifest.0d2210dfed2c4eb3c372e220b7be540e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d409fda1a5b6021b834344c79549ad",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "84cd22dfa5229ec31e3b",
+    "url": "/to-do-list-react/static/css/main.7675bc9e.chunk.css"
+  },
+  {
+    "revision": "21cccf955065d306d159",
+    "url": "/to-do-list-react/static/js/2.f58a2cb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list-react/static/js/2.f58a2cb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84cd22dfa5229ec31e3b",
+    "url": "/to-do-list-react/static/js/main.22c8dad5.chunk.js"
+  },
+  {
+    "revision": "46b5cffd45b6b28600f3",
+    "url": "/to-do-list-react/static/js/runtime-main.43f91b30.js"
+  }
+]);
