1156f09caae12e028405efb8eabb68e811f46d3d
Wojciech Knott <wojciechknott@gmail.com>
2020-07-20 22:43:43 +0200

    Updates

--- precache-manifest.30e111faff1a27450f434002914b5f86.js
+++ precache-manifest.30e111faff1a27450f434002914b5f86.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8812fe7064707075076e9e29acdfe0d2",
+    "revision": "a6510cf5a65e64ea04a3c2c567e5445e",
     "url": "/to-do-list-react/index.html"
   },
   {
-    "revision": "96fe4fbd4c1866046ef8",
-    "url": "/to-do-list-react/static/css/main.fba17aa7.chunk.css"
+    "revision": "aaaafabf75e084998bc4",
+    "url": "/to-do-list-react/static/css/main.4c219994.chunk.css"
   },
   {
-    "revision": "70b5e74f9efff2ddafa3",
-    "url": "/to-do-list-react/static/js/2.c337123f.chunk.js"
+    "revision": "ecc75fa6474a90df6399",
+    "url": "/to-do-list-react/static/js/2.7902ff7d.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/to-do-list-react/static/js/2.c337123f.chunk.js.LICENSE.txt"
+    "url": "/to-do-list-react/static/js/2.7902ff7d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "96fe4fbd4c1866046ef8",
-    "url": "/to-do-list-react/static/js/main.90fe4aa9.chunk.js"
+    "revision": "aaaafabf75e084998bc4",
+    "url": "/to-do-list-react/static/js/main.903c9cd3.chunk.js"
   },
   {
     "revision": "46b5cffd45b6b28600f3",
