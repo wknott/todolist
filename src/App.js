c1bdf47ca4cb04251d81bdc731fe1b9aabe5af01
Wojciech Knott <wojciechknott@gmail.com>
2020-08-12 23:20:58 +0200

    added useTasks hook

--- src/App.js
+++ src/App.js
@@ -1,63 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Form from "./Form";
 import Buttons from "./Buttons";
 import Tasks from "./Tasks";
 import Section from "./Section";
 import Container from "./Container";
+import { useTasks } from "./useTasks";
 
 const App = () => {
-  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
   const [hideDoneTasks, setHideDoneTasks] = useState(false);
-  const [directionOfSort, setDirectionOfSort] = useState(null);
-
-  const getHighestId = () => Math.max(...tasks.map(({ id }) => id));
-
-  const addTask = (name) => {
-    setTasks(tasks => [
-      ...tasks,
-      {
-        id: tasks.length ? getHighestId() + 1 : 1,
-        name,
-        done: false,
-      }
-    ]);
-  };
-
-  const removeTask = (id) => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = (id) => {
-    setTasks(tasks => tasks.map(task => {
-      return task.id === id ? ({
-        ...task,
-        done: !task.done
-      }) : (task);
-    }));
-  };
 
   const togglehHideDoneTasks = () => {
     setHideDoneTasks(!hideDoneTasks);
   };
 
-  const markAllTasksAsDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task,
-      done: true,
-    })));
-  };
-
-  const sortTasks = () => {
-    setDirectionOfSort(directionOfSort === "asc" ? "desc" : "asc");
-    setTasks(tasks => [...tasks].sort(
-      (a, b) => directionOfSort === "asc" ? b.name.localeCompare(a.name) : a.name.localeCompare(b.name)
-    ));
-  };
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks])
+  const {
+    tasks,
+    directionOfSort,
+    addTask,
+    removeTask,
+    toggleTaskDone,
+    markAllTasksAsDone,
+    sortTasks
+  } = useTasks();
 
   return (
     <Container>
---
+++ src/useTasks.js
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+
+export const useTasks = () => {
+  const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
+  const [directionOfSort, setDirectionOfSort] = useState(null);
+  const getHighestId = () => Math.max(...tasks.map(({ id }) => id));
+
+  const addTask = (name) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        id: tasks.length ? getHighestId() + 1 : 1,
+        name,
+        done: false,
+      }
+    ]);
+  };
+
+  const removeTask = (id) => {
+    setTasks(tasks => tasks.filter(task => task.id !== id));
+  };
+
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      return task.id === id ? ({
+        ...task,
+        done: !task.done
+      }) : (task);
+    }));
+  };
+
+  const markAllTasksAsDone = () => {
+    setTasks(tasks => tasks.map(task => ({
+      ...task,
+      done: true,
+    })));
+  };
+
+  const sortTasks = () => {
+    setDirectionOfSort(directionOfSort === "asc" ? "desc" : "asc");
+    setTasks(tasks => [...tasks].sort(
+      (a, b) => directionOfSort === "asc" ? b.name.localeCompare(a.name) : a.name.localeCompare(b.name)
+    ));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return {
+    tasks,
+    directionOfSort,
+    addTask,
+    removeTask,
+    toggleTaskDone,
+    markAllTasksAsDone,
+    sortTasks
+  };
+};
