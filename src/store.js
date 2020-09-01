4691d08ff99d8cedc00bdf1896c1c91996a49735
Wojciech Knott <wojciechknott@gmail.com>
2020-09-01 18:38:03 +0200

    added localStorage tasks save

--- src/store.js
+++ src/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/tasksSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     tasks: tasksReducer,
   },
 });
+
+store.subscribe(() => {
+  console.log(store.getState().tasks.tasks)
+  localStorage.setItem("tasks", JSON.stringify(store.getState().tasks.tasks))
+})
+export default store;
