89348145119e7c6541b0b02ccc601af837144e7d
Wojciech Knott <wojciechknott@gmail.com>
2020-09-17 17:31:57 +0200

    added ExampleTasksButton component

---
+++ src/features/tasks/ExampleTasksButton/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Button from "../Button";
+import { fetchExampleTasks, selectLoading } from "../tasksSlice";
+
+const ExampleTasksButton = () => {
+  const dispatch = useDispatch();
+  const loading = useSelector(selectLoading);
+  return (
+    <Button disabled={loading} onClick={() => dispatch(fetchExampleTasks())}>
+      {loading ? "Ładowanie..." : "Pobierz przykładowe zadania"}
+    </Button>
+  )
+}
+
+export default ExampleTasksButton;
