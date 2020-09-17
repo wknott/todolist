f44060a2aa2ebece3871f7fc78110cdce73dac38
Wojciech Knott <wojciechknott@gmail.com>
2020-09-17 17:31:23 +0200

    removed hover and active filter when button disabled

--- src/features/tasks/Buttons/styled.js
+++ src/features/tasks/Buttons/styled.js
@@ -6,23 +6,3 @@ export const ButtonsContainer = styled.div`
     grid-gap: 20px;
   }
 `;
-
-export const Button = styled.button`
-  padding: 0 10px;
-  border: none;
-  background-color: transparent;
-  color: ${({ theme }) => theme.colors.primary};
-  transition: 0.5s;
-
-  &:hover {
-    filter: brightness(115%);
-  }
-
-  &:active {
-    filter: brightness(130%);
-  }
-
-  &:disabled {
-    color: ${({ theme }) => theme.colors.secondary};
-  }
-`;
