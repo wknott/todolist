44d9daa954c2cb02fd6b4661cca799d0a9f290d2
Wojciech Knott <wojciechknott@gmail.com>
2020-09-17 18:23:44 +0200

    installed react-router-dom and added BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
 import Tasks from "./features/tasks/Tasks";
 
-export default () => <Tasks />
+export default () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">
+            Zadania
+          </Link>
+        </li>
+        <li>
+          <Link to="/autor">
+            O autorze
+          </Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/zadania">
+        <Tasks />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+)
