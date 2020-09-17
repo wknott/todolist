66de772bc35374456f9e5c77eb0b08584f5fdc14
Wojciech Knott <wojciechknott@gmail.com>
2020-09-17 21:59:54 +0200

    renamed component name

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -2,23 +2,22 @@ import React from "react";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
 import Container from "../../common/Container";
-import Paragraph from "./Paragraph";
 
-const Author = () => (
+const AuthorPage = () => (
   <Container>
     <Header title="O autorze" />
     <Section
       title="Wojciech Knott"
       sectionBody={
-        <Paragraph>
+        <>
           Obecnie kończę studia magisterskie na kierunku Automatyka i Robotyka.
           Programowaniem zajmuję się od wielu lat. W tym czasie poznałem podstawy kilku
           języków programowania i technologii, ale to programowanie <strong>aplikacji webowych </strong>
           sprawia mi największą frajdę!
-        </Paragraph>
+        </>
       }
     />
   </Container>
 );
 
-export default Author;
+export default AuthorPage;
