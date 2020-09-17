46494df92e16788fc07071609f62e61606fcf52d
Wojciech Knott <wojciechknott@gmail.com>
2020-09-17 17:30:36 +0200

    moved Button styles to separate component

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { ButtonsContainer, Button } from "./styled";
-import { selectTasks, toggleHideDoneTasks, markAllTasksAsDone, toggleSortDirection } from "../tasksSlice";
+import { ButtonsContainer } from "./styled";
+import Button from "../Button/"
+import {
+  selectTasksState,
+  toggleHideDoneTasks,
+  markAllTasksAsDone,
+  toggleSortDirection,
+} from "../tasksSlice";
 
 const Buttons = () => {
-  const { tasks, hideDoneTasks, sortDirection } = useSelector(selectTasks);
+  const { tasks, hideDoneTasks, sortDirection } = useSelector(selectTasksState);
   const dispatch = useDispatch();
 
   return (
