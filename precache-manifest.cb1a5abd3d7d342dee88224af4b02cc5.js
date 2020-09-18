f7ce84a5d08890c22bf270c8ffdb68d44dfd6371
Wojciech Knott <wojciechknott@gmail.com>
2020-09-18 12:35:16 +0200

    Updates

--- precache-manifest.cb1a5abd3d7d342dee88224af4b02cc5.js
+++ precache-manifest.cb1a5abd3d7d342dee88224af4b02cc5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "beb9ef4f20895f183e29946cd526a42b",
+    "revision": "1be33e11ad3fad6bd68ac14bc3543adc",
     "url": "/to-do-list-react/index.html"
   },
   {
-    "revision": "0656395362dd2533f289",
-    "url": "/to-do-list-react/static/js/2.96536951.chunk.js"
+    "revision": "c125eaba8ecaa22c3777",
+    "url": "/to-do-list-react/static/js/2.d161cc55.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/to-do-list-react/static/js/2.96536951.chunk.js.LICENSE.txt"
+    "url": "/to-do-list-react/static/js/2.d161cc55.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9c21ff0d0a754e9bd662",
-    "url": "/to-do-list-react/static/js/main.9c4bfa21.chunk.js"
+    "revision": "c5816c2f6d26d06bef88",
+    "url": "/to-do-list-react/static/js/main.48d553c1.chunk.js"
   },
   {
     "revision": "46b5cffd45b6b28600f3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/to-do-list-react/precache-manifest.b07723cf15e358a93baf296bbaee60d3.js"
+  "/to-do-list-react/precache-manifest.cb1a5abd3d7d342dee88224af4b02cc5.js"
 );
 
 self.addEventListener('message', (event) => {
